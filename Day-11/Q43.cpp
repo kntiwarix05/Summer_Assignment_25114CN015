188280897ba8efab2c06974df3b60f3ef08b9f99
kntiwarix05 <karunanidhitiwari05@gmail.com>
2026-06-11 12:00:00 +0530

    Day-11 uplodaded.

---
+++ Day-11/Q43.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num) {
+    if (num <= 1)
+        return false;
+
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isPrime(num))
+        cout << num << " is a Prime Number";
+    else
+        cout << num << " is Not a Prime Number";
+
+    return 0;
+}

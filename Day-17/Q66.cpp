39605a396ad45d1517b1bd717c785ada2d9e86b5
kntiwarix05 <karunanidhitiwari05@gmail.com>
2026-06-17 11:43:16 +0530

    Day-17 uplodaded.

---
+++ Day-17/Q66.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1, n2;
+
+    cout << "Enter size of first array: ";
+    cin >> n1;
+    int arr1[n1];
+
+    cout << "Enter elements of first array:\n";
+    for (int i = 0; i < n1; i++) {
+        cin >> arr1[i];
+    }
+
+    cout << "Enter size of second array: ";
+    cin >> n2;
+    int arr2[n2];
+
+    cout << "Enter elements of second array:\n";
+    for (int i = 0; i < n2; i++) {
+        cin >> arr2[i];
+    }
+
+    int uni[n1 + n2];
+    int k = 0;
+
+    // Add elements of first array
+    for (int i = 0; i < n1; i++) {
+        uni[k++] = arr1[i];
+    }
+
+    for (int i = 0; i < n2; i++) {
+        bool found = false;
+
+        for (int j = 0; j < k; j++) {
+            if (arr2[i] == uni[j]) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            uni[k++] = arr2[i];
+        }
+    }
+
+    cout << "Union of arrays: ";
+    for (int i = 0; i < k; i++) {
+        cout << uni[i] << " ";
+    }
+
+    return 0;
+}
